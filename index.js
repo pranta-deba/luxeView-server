497d384e7a8287826f4f8252d09a7d916be2b672
Pranto Deb <rajprit0m@outlook.com>
2024-08-15 04:29:16 +0600

    [Latest Product Api] : created latest 10 products api route and a-z sorting the 10 product

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv");
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kdhebsc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Middleware
 app.use(cors());
@@ -13,6 +15,39 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("luxe-view");
+    const productCollection = db.collection("products");
+
+    // latest products
+    app.get("/latest", async (req, res) => {
+      const latestProducts = await productCollection
+        .find()
+        .sort({ productCreationDateTime: -1 })
+        .limit(10)
+        .toArray();
+      res.json(latestProducts);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 // Start the server
 app.listen(PORT, () => {
