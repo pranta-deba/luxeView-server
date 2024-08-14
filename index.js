f7f2911e794f771234a41227fa7ed92ff5327674
Pranto Deb <rajprit0m@outlook.com>
2024-08-14 20:39:13 +0600

    [Backend Setup] : Install express, cors, dotenv, jsonwebtoken and mongodb

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const PORT = process.env.PORT || 5000;
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Basic route
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
